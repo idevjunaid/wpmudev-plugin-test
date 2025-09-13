2025b40d661627e182190c1bb6ad140721849b5b
devmoin <idevmoin@gmail.com>
2025-09-13 15:59:29 +0500

    styling update

--- assets/js/drivetestpage.min.asset.php
+++ assets/js/drivetestpage.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element'), 'version' => 'dc0714d949baa2b6022c');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element'), 'version' => 'a47f9b3a42de6b1bb8b4');
